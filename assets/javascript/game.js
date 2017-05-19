7edc5adaf8247819170b74e79f4abf57b28f5f08
cherisek27 <cheriseknox27@gmail.com>
2017-05-19 14:41:38 -0400

    homework 4

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,54 +1,62 @@
-var wordsList = ["mickey","minnie","goofy","winnie the pooh","cinderella"];  
+  var number;
+  var gem = [];
+  var total = 0;
+  var winCounter = 0;
+  var loseCounter = 0;
 
-var wins = 0; 
-var losses = 0; 
-var guessesLeft = 10; 
-var guessedLetters = []; 
-var lettertoGuess = null;  
-
-	var userGuess = wordsList[Math.floor(Math.random() * wordsList.length)]; 
-
-	var updateGuessesLeft = function() {
-		document.querySelector("#guessesLeft").innerHTML ="Guesses Left:" + guessesLeft;
-	};  
-
-	var updatedGuessSoFar = function() {
-		this.lettertoGuess = this.wordsList[Math.floor(Math.random() * this.wordsList.length)];
-	};
-
-	var updatedGuessesSoFar = function() {
-		document.querySelector("#let").innerHTML = "Your Guesses so far:" + guessedLetters.join(",");
-	}; 
-
-	var reset = function() {
-		totalGuesses = 10; 
-		guessesLeft = 10; 
-		updatedGuessesSoFar(); 
-	}
-
-	updateLetterToGuess(); 
-	updateGuessesLeft(); 
-
-	document.onekeyup = function(event) {
-		guessesLeft--;
-		var userGuess = String.fromCharCode(event.keyCode).toLowerCase(); 
-
-		guessedLetters.push(userGuess);
-		updateGuessesLeft();
-		updatedGuessesSoFar(); 
-
-			if (guessesLeft > 0){
-				if (userGuess == letterToGuess){
-					wins++; 
-					document.querySelector("#wins").innerHTML = "Wins: " + wins; 
-					alert("You win!");
-					reset();
-				}
-			  }else if (guessesLeft == 0){
-				losses++;
-				document.querySelector("losses").innerHTML = "Losses: " + losses;
-				alert("Game Over!");
-				reset();
-			}
-		
-	};
+  $(document).ready(function() {
+    function reset() {
+    
+    gem = [];
+    
+    number = Math.floor((Math.random() * 101) + 19);
+    $("#givenNumber").text(number);
+      
+    
+        for (var i = 0; i < 4; i++) {
+            gem[i] = Math.floor((Math.random() * 11) + 1);
+            
+        var repeat = true;
+                while (repeat) {
+                    repeat = false;
+                    for (var j = 0; j < i; j++) {
+                        if (gem[i] === gem[j]) {
+                            repeat = true;
+                            gem[i] = Math.floor((Math.random() * 11) + 1);
+                        }
+                    }
+                }
+      gem.push(gem[i]);
+           
+    }
+    total = 0;
+    $("#total").html(total);
+    }
+    
+    reset();  
+    $("#gem1").val(gem[0]);
+    $("#gem2").val(gem[1]);
+    $("#gem3").val(gem[2]);
+    $("#gem4").val(gem[3]);
+    
+    $("button").click(function() {
+      console.log(this);
+      total = parseInt(total) + parseInt($(this).val());  
+      $("#total").html(total);
+      
+      if (total == number) {
+      winCounter++;
+      $("#wins").html("Wins: " + winCounter);
+      setTimeout(reset, 1000); 
+      alert("You won!");
+      } 
+      else if (total > number) {
+        loseCounter++;
+      $("#losses").html("Losses: " + loseCounter);
+      setTimeout(reset, 1000);
+      alert("Sorry,you lose!");
+      }
+    });
+    
+  
+  });
